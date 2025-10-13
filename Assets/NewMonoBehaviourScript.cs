1ca6a199ad5e7014a0969e5e0b1ab8d43ccb84be
ahernandez <alejandro.martinezhernandez@student.htw-berlin.de>
2025-10-13 13:55:51 +0200

    woao

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input.GetAxis("Vertical");
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
